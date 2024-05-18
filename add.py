7c15d73841ee6335ef817dea49c755f731906962
Tejas Dalvi <118128520+tejasdalvi9@users.noreply.github.com>
2024-05-18 15:00:03 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,3 +1,5 @@
-a=int(input("Enter a number : "))
-b=int(input("Enter 2nd number : "))
+a=int(input("Enter a number : ")) # this is modified in local
+b=int(input("Enter 2nd number : ")) #this is modified in github
 print("Addition : ",a+b)
+#this is line is in local and this is modified in github
+#this is not in local
