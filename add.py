eea39b93f7cd96885ac500ef53ff83a66cd3d553
tejas <tejas@gmail.com>
2024-05-18 15:02:15 +0530

    modified add for conflict

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-a=int(input("Enter a number : this is new"))
-b=int(input("Enter 2nd number : "))
+a=int(input("Enter a number : ")) #modified in local 
+b=int(input("Enter 2nd number : ")) 
 print("Addition : ",a+b)
-print("line added by local")
+print("line added by local") #added in local
